680cab3a6365e9a88d1b9060458d79fdc86f673e
Lalit Kumar <sunil22006@gmail.com>
2026-08-14 13:37:07 +0530

    Added navbar feature

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,6 +1,10 @@
 const Navbar = () => {
   return (
-    <div className="h-14 flex items-center justify-center px-5 border-b border-white/6 bg-black gap-">Navbar</div>
+    <div className="h-14 flex items-center justify-center px-5 border-b border-white/6 bg-black gap-">
+      <div className="px-5">
+
+      </div>
+    </div>
   )
 }
 
